75d61396c3367fa2ad91733cb8598a034fdca844
serizao <serizao@users.noreply.github.com>
2015-07-10 15:57:11 +0200

    Update edit.php

--- admin/edit.php
+++ admin/edit.php
@@ -55,7 +55,7 @@ elseif ($_GET['edition']){
 				<label for="title">Titre de l´article</label><br>
 				<input id="title" name="title" <?php if ($metaData['title']) echo 'value="'.$metaData['title'].'"';?>><br>
 				<label for"description">En-tête (sans retour à la ligne)</label><br>
-<textarea id="description" name="description" rows="5" cols="100"><?php if (isset($description)) echo $description;?></textarea><br>
+				<textarea id="description" name="description" rows="5" cols="100"><?php if (isset($description)) echo $description;?></textarea><br>
 				<label for="content">Contenu</label><br>
 				<textarea id="content" rows="25" cols="100" name="content"><?php if (isset($content)) echo "$content";?></textarea><br>
 				<input class="input" type="submit" value="Valider">
